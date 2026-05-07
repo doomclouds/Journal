09aa2413dbd5b98a34e279e8d8d75b4a89f3a70e
palink <1006282023@qq.com>
2026-05-08 00:41:48 +0800

    chore(desktop): add electron react skeleton

---
+++ apps/desktop/electron/main.cjs
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1180,
+    height: 780,
+    minWidth: 960,
+    minHeight: 640,
+    title: "Journal",
+    backgroundColor: "#f6efe4",
+    webPreferences: {
+      preload: path.join(__dirname, "preload.cjs"),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  if (isDev) {
+    mainWindow.loadURL("http://localhost:5173");
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
